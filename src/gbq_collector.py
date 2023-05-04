3dbef2878f47fec5d64b88f14755104e952abead
crahal <charlierahal@gmail.com>
2023-05-04 10:53:39 +0100

    Final version of GBQ collector.

--- src/gbq_collector.py
+++ src/gbq_collector.py
@@ -1,6 +1,6 @@
 import os
-#import re
-#import ast
+import re
+import ast
 import traceback
 import pandas as pd
 from google.cloud import bigquery
@@ -62,7 +62,7 @@ def get_data_from_lists(query_list, client, query_type):
                 ]
             )
             QUERY = """
-                    SELECT id, title.preferred, doi, journal.issn,
+                    SELECT id, title.preferred, doi, journal.issn, journal.eissn,
                     type, date_normal, category_for,
                     citations_count, research_org_cities,
                     research_org_country_names,
@@ -80,7 +80,7 @@ def get_data_from_lists(query_list, client, query_type):
 def get_all_data(chunk_size, file_path, client, query_list, query_type):
     """ Helper function to get all data from iterative queries"""
     if chunk_size < len(query_list):
-        print(f'We have {len(query_list)} ISSNs with from chunksize {chunk_size}')
+        print(f'We have {len(query_list)} {query_type}s with chunksize {chunk_size}')
         for chunk in chunker(query_list, chunk_size):
             results = get_data_from_lists(chunk,
                                           client,
@@ -95,30 +95,30 @@ def get_all_data(chunk_size, file_path, client, query_list, query_type):
         print('Bad Chunksize')
 
 
-#def get_all_refs(df):
-#    """ Get all references from a pubid dataframe"""
-#    all_refs = []
-#    for element in df['reference_ids'].to_list():
-#        element = element.replace('\n','')
-#        element = element.replace('[', '')
-#        element = element.replace(']', '')
-#        element = element.replace("'", '')
-#        element = element.split(' ')
-#        for ele in element:
-#            if len(ele) > 0:
-#                all_refs.append(ele)
-#    return list(set(all_refs))
+def get_all_refs(df):
+    """ Get all references from a pubid dataframe"""
+    all_refs = []
+    for element in df['reference_ids'].to_list():
+        element = element.replace('\n','')
+        element = element.replace('[', '')
+        element = element.replace(']', '')
+        element = element.replace("'", '')
+        element = element.split(' ')
+        for ele in element:
+            if len(ele) > 0:
+                all_refs.append(ele)
+    return list(set(all_refs))
 
 
-#def get_all_citations(df):
-#    """ Get all citations from a pubid dataframe"""
-#    all_citations = []
-#    for element in df['citations'].to_list():
-#        if len(element) > 2:
-#            for ele in element.split('\n'):
-#                x = ast.literal_eval(re.search('({.+})', ele).group(0))
-#                all_citations.append(x['id'])
-#    return list(set(all_citations))
+def get_all_citations(df):
+    """ Get all citations from a pubid dataframe"""
+    all_citations = []
+    for element in df['citations'].to_list():
+        if len(element) > 2:
+            for ele in element.split('\n'):
+                x = ast.literal_eval(re.search('({.+})', ele).group(0))
+                all_citations.append(x['id'])
+    return list(set(all_citations))
 
 def load_dimensions_returns(path):
     return pd.read_csv(path,
@@ -171,28 +171,25 @@ def main():
                      issns_to_query,
                      'issn')
 
-    # Lets evaluate our cache here
-    from_dim_issn = load_dimensions_returns(dim_issn_out_path)
-    basic_coverage(raw_data, from_dim_issn, year)
 
+    all_pubs_from_issn = pd.read_csv(dim_issn_out_path,
+                                     usecols=[1, 13, 14],
+                                     names=['id',
+                                            'reference_ids',
+                                            'citations'])
 
-    #all_pubs_from_issn = pd.read_csv(file_path,
-    #                                 usecols=[1, 15, 16],
-    #                                 names=['id',
-    #                                        'reference_ids',
-    #                                        'citations'])
-    #print('Total number of papers', len(all_pubs_from_issn))
-    #all_refs = get_all_refs(all_pubs_from_issn)
-    #print('Total references to query: ', len(all_refs))
-    #all_citations = get_all_citations(all_pubs_from_issn)
-    #print('Total citations to query: ', len(all_citations))
-    #file_name = 'references_of_all_pubs.csv'
-    #file_path = os.path.join(dim_out, file_name)
-    #num_pubids = 300000 # hardcoded to circumvent limits
-    #get_all_data(num_pubids, file_path, client, all_refs, 'article')
-    #file_name = 'citations_of_all_pubs.csv'
-    #file_path = os.path.join(dim_out, file_name)
-    #get_all_data(num_pubids, file_path, client, all_citations, 'article')
+    print('Total number of papers', len(all_pubs_from_issn))
+    all_refs = get_all_refs(all_pubs_from_issn)
+    print('Total references to query: ', len(all_refs))
+    all_citations = get_all_citations(all_pubs_from_issn)
+    print('Total citations to query: ', len(all_citations))
+    file_name = 'references_of_all_pubs.csv'
+    file_path = os.path.join(dim_out, file_name)
+    num_pubids = 300000 # hardcoded to circumvent limits
+    get_all_data(num_pubids, file_path, client, all_refs, 'article')
+    file_name = 'citations_of_all_pubs.csv'
+    file_path = os.path.join(dim_out, file_name)
+    get_all_data(num_pubids, file_path, client, all_citations, 'article')
 
 
 if __name__ == '__main__':
